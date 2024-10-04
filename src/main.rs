dd9b172efec00a761b13d98d51ea47581fb7af11
Espen Eika <espen.h.eika@gmail.com>
2024-10-04 09:42:06 +0200

    Change of name

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
 use std::alloc::System;
 use std::env;
+use std::os::unix::thread;
+use std::thread::sleep;
 use std::time::{Duration, SystemTime};
+
+struct FocusPeriod {
+    interval: Duration,
+    purpose: FocusPurpose,
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    let focus_periode = FocusPeriod {
+        interval: Duration::new(10, 0),
+        purpose: FocusPurpose::Work,
+    };
     let time_now = SystemTime::now();
-    for i in args {
-        println!("{i}");
-        println!("{i}");
+
+    let check_time = Duration::new(1, 0);
+
+    while focus_periode.interval.as_secs() > time_now.elapsed().unwrap().as_secs() {
+        std::thread::sleep(check_time);
     }
-    println!("{:?}", time_now.elapsed().unwrap().as_nanos());
+    println!("Done!");
+}
+
+enum FocusPurpose {
+    Work,
+    Study,
+    Mindfullness,
 }
